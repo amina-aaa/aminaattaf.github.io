d6ecfed1c6e94bef39f8380393466f1a69db7d55
amina-aaa <aminaattaf31@gmail.com>
2025-10-28 12:54:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Petit effet de défilement fluide
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
